bb49dff402aa191d38de1854b95144ea7b244728
kumaraditya62 <kumaraditya10vaishali@gmail.com>
2025-09-21 09:10:57 +0530

    commiting changes

--- login.js
+++ login.js
@@ -1,6 +1,10 @@
-const container=document.querySelector('.container');
-const LoginLink=document.querySelector('.signInLink')
-const RegisterLink=document.querySelector('.signUpLink')
-RegisterLink.addEventListener('click',()=>{
+const container = document.querySelector('.container');
+const LoginLink = document.querySelector('.SignInLink');
+const RegisterLink = document.querySelector('.SignUpLink');
+
+RegisterLink.addEventListener('click', () =>{
     container.classList.add('active');
+})
+LoginLink.addEventListener('click', () => {
+    container.classList.remove('active');
 })
